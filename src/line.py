009bf714c9a8e46c9fa80c8b0c4b2363187e8b9b
lvhanh270597 <=>
2017-11-17 21:03:12 +0700

    fix file: line, segment, ..

--- src/line.py
+++ src/line.py
@@ -6,11 +6,4 @@ class Line(object):
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
-        self.c = c
-    def  __init__(self):
-        self.a = random.randint(0, 10)
-        self.b = random.randint(0, 10)
-        while (self.a == 0 and self.b == 0):
-            self.a = random.randint(0, 10)
-            self.b = random.randint(0, 10)
-        
+        self.c = c    
